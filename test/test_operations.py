24ed1626fa4c19367016441d7490ec9694afefaa
Daniel Furman <dryanfurman@gmail.com>
2021-03-30 13:16:58 -0700

    Update test_operations.py

--- test/test_operations.py
+++ test/test_operations.py
@@ -11,7 +11,7 @@ DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 test_data = [
     os.path.join(DATA, x)
     for x in ['feature-correlations-test.csv','feature-importance-test.csv',
-        'raw-data-test.csv']]
+        'raw-data-test.csv', 'cov.csv']]
 
 # Unit tests for the feature selection algorithm.
 covariance_org = pd.read_csv(test_data[0])
@@ -29,10 +29,8 @@ from RecFeatureSelect import feature_selector
 def test_function():
     # Second, assert that all final correlations are lower than the threshold
     feature_selector(covariance_org, feature_importance, threshold, raw_data)
-    # Grab the final output
-    test_output_cov = 'src/RecFeatureSelect/data/cov.csv'
-    # Convert into numpy array so we can fill diag with zeros
-    cov = pd.read_csv(test_output_cov, index_col = "Unnamed: 0")
+    # Grab the final output and convert into numpy array so we can fill diag with zeros
+    cov = pd.read_csv(test_data[3], index_col = "Unnamed: 0")
     cov = cov.to_numpy()
     np.fill_diagonal(cov, 0)
     # did the algorithm remove correlated pairs above the threshold? :
