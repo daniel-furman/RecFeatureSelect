bc2b016e37cc8aa7499bb47925c727435d432e2d
Daniel Furman <dryanfurman@gmail.com>
2021-03-30 12:33:44 -0700

    Add files via upload

--- test/test_operations.py
+++ test/test_operations.py
@@ -5,28 +5,29 @@
 
 import numpy as np
 import pandas as pd
+import os
+
+DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+test_data = [
+    os.path.join(DATA, x)
+    for x in ['feature-correlations-test.csv','feature-importance-test.csv',
+        'raw-data-test.csv']]
 
 # Unit tests for the feature selection algorithm.
-# Set up data for the function
-
-covariance_org = pd.read_csv('data/feature-correlations-test.csv')
-feature_importance = pd.read_csv('data/feature-importance-test.csv')
+covariance_org = pd.read_csv(test_data[0])
+feature_importance = pd.read_csv(test_data[0])
 threshold = 0.85
-raw_data = pd.read_csv('data/raw-data-test.csv')
-
-print(covariance_org.columns)
-print(feature_importance.columns)
+raw_data = pd.read_csv(test_data[0])
 
 def test_inputs():
     # First assert that the inputs have the same features (and order)
     assert np.all(covariance_org.columns == feature_importance.columns)
     assert list(covariance_org) == list(feature_importance)
 
-from RecFeatureSelect._main import RecFeatureSelect
-
 def test_function():
     # Second, assert that all final correlations are lower than the threshold
-    RecFeatureSelect(covariance_org, feature_importance, threshold, raw_data)
+    from RecFeatureSelect import feature_selector
+    feature_selector(covariance_org, feature_importance, threshold, raw_data)
     cov = pd.read_csv('data/cov.csv', index_col = "Unnamed: 0")
     cov = cov.to_numpy()
     np.fill_diagonal(cov, 0)
